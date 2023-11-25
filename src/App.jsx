9149fd6dd43b33bb6f6d55a6204f81619f309a0a
Nayrb201429 <bryan201429@gmail.com>
2023-11-24 23:05:06 -0500

    csvTested02

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import { Route,Routes } from 'react-router-dom'
 import Home from './views/Home/Home'
-
+import Radar from './views/Radar/Radar'
 function App() {
 
   return (
     <>
      <Routes>
       <Route path='/' element={<Home></Home>}></Route>
+      <Route path='/Radar' element={<Radar></Radar>}></Route>
      </Routes>
     </>
   )
